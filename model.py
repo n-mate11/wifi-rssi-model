adf31d283a2b2aef9b969403a2671e2f0093f196
Máté Nagy <nagymatepeter@student.vu.nl>
2023-06-07 23:50:25 +0200

    Move the tuner to a separate file

--- model.py
+++ model.py
@@ -17,18 +17,16 @@ from sklearn.multioutput import MultiOutputRegressor
 
 # flags
 USE_DIRECTION_FLAG = False
-TRAIN_NN_FLAG = True
-TRAIN_ML_FLAG = False
+TRAIN_NN_FLAG = False
+TRAIN_ML_FLAG = True
 USE_PLOT_FLAG = False
 USE_COORDS_FLAG = False
 
 if TRAIN_NN_FLAG:
-    import tensorflow as tf
-    from tensorflow import keras
-    from keras.layers import Dense, Dropout, BatchNormalization, Flatten, Input
+    from keras.layers import Dense, BatchNormalization
     from keras.models import Sequential
-    from keras.optimizers import Adam
     import keras_tuner as kt
+    from tuner import MyHyperModel
 
 import matplotlib.pyplot as plt
 
@@ -218,49 +216,6 @@ def plot_3d(y_test, y_pred):
     ax.set_zlabel("z")
     ax.set_title("Actual values vs Predicted values")
     plt.show()
-
-
-class MyHyperModel(kt.HyperModel):
-    def build(self, hp):
-        model = Sequential()
-        model.add(Flatten())
-        # Wetther to use normalization
-        if hp.Boolean("normalization"):
-            model.add(BatchNormalization())
-
-        # Tune the number of layers.
-        for i in range(hp.Int("num_layers", 1, 5)):
-            model.add(
-                Dense(
-                    # Tune number of units separately.
-                    units=hp.Int(f"units_{i}", min_value=16, max_value=512, step=32),
-                    activation="relu",
-                )
-            )
-        if hp.Boolean("dropout"):
-            model.add(Dropout(rate=0.25))
-        model.add(Dense(units=3, activation="linear"))
-
-        # optimize learning rate
-        hp_learning_rate = hp.Choice("learning_rate", values=[1e-2, 1e-3, 1e-4])
-        model.compile(
-            optimizer=Adam(learning_rate=hp_learning_rate),
-            loss="mean_squared_error",
-            metrics=["mse", "mae"],
-        )
-
-        return model
-
-    def fit(self, hp, model, *args, **kwargs):
-        return model.fit(
-            *args,
-            **kwargs,
-            batch_size=hp.Int(
-                "batch_size", min_value=8, max_value=MAX_BATCH_SIZE, step=8
-            ),
-            epochs=hp.Int("epochs", min_value=5, max_value=MAX_EPOCHS, step=10),
-            shuffle=hp.Boolean("shuffle"),
-        )
 
 
 def main():
