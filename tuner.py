469642db90a58417d1b631e87f8122490d1cabd1
Máté Nagy <nagymatepeter@student.vu.nl>
2023-06-10 12:55:03 +0200

    Train DL models

--- tuner.py
+++ tuner.py
@@ -3,8 +3,27 @@ from keras.models import Sequential
 from keras.optimizers import Adam
 import keras_tuner as kt
 
+MIN_EPOCHS = 5
 MAX_EPOCHS = 305
+STEP_EPOCHS = 10
+
+MIN_BATCH_SIZE = 8
 MAX_BATCH_SIZE = 128
+STEP_BATCH_SIZE = 8
+
+MIN_LAYERS = 1
+MAX_LAYERS = 5
+
+MIN_UNITS = 8
+MAX_UNITS = 512
+STEP_UNITS = 8
+
+MIN_DROPOUT = 0.0
+MAX_DROPOUT = 0.5
+STEP_DROPOUT = 0.05
+DEFAULT_DROPOUT = 0.25
+
+LEARNING_RATES = [1e-2, 1e-3, 1e-4]
 
 
 class MyHyperModel(kt.HyperModel):
@@ -16,20 +35,35 @@ class MyHyperModel(kt.HyperModel):
             model.add(BatchNormalization())
 
         # Tune the number of layers.
-        for i in range(hp.Int("num_layers", 1, 5)):
+        for i in range(hp.Int("num_layers", MIN_LAYERS, MAX_LAYERS)):
             model.add(
                 Dense(
                     # Tune number of units separately.
-                    units=hp.Int(f"units_{i}", min_value=16, max_value=512, step=32),
+                    units=hp.Int(
+                        f"units_{i}",
+                        min_value=MIN_UNITS,
+                        max_value=MAX_UNITS,
+                        step=STEP_UNITS,
+                    ),
                     activation="relu",
                 )
             )
         if hp.Boolean("dropout"):
-            model.add(Dropout(rate=0.25))
+            model.add(
+                Dropout(
+                    rate=hp.Float(
+                        "dropout_rate",
+                        min_value=MIN_DROPOUT,
+                        max_value=MAX_DROPOUT,
+                        default=DEFAULT_DROPOUT,
+                        step=STEP_DROPOUT,
+                    )
+                )
+            )
         model.add(Dense(units=3, activation="linear"))
 
         # optimize learning rate
-        hp_learning_rate = hp.Choice("learning_rate", values=[1e-2, 1e-3, 1e-4])
+        hp_learning_rate = hp.Choice("learning_rate", values=LEARNING_RATES)
         model.compile(
             optimizer=Adam(learning_rate=hp_learning_rate),
             loss="mean_squared_error",
@@ -43,8 +77,13 @@ class MyHyperModel(kt.HyperModel):
             *args,
             **kwargs,
             batch_size=hp.Int(
-                "batch_size", min_value=8, max_value=MAX_BATCH_SIZE, step=8
+                "batch_size",
+                min_value=MIN_BATCH_SIZE,
+                max_value=MAX_BATCH_SIZE,
+                step=STEP_BATCH_SIZE,
             ),
-            epochs=hp.Int("epochs", min_value=5, max_value=MAX_EPOCHS, step=10),
+            epochs=hp.Int(
+                "epochs", min_value=MIN_EPOCHS, max_value=MAX_EPOCHS, step=STEP_EPOCHS
+            ),
             shuffle=hp.Boolean("shuffle"),
         )
